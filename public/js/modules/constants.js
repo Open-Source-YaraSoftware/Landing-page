40c00418fea99d1f830bb6b943665ddb16008180
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-09-08 17:50:12 -0500

    feat: add language switch functionality and static spanish page

--- public/js/modules/constants.js
+++ public/js/modules/constants.js
@@ -3,6 +3,7 @@ export const menuOptions = document.querySelector('.navbar__complete-list');
 export const navbar = document.querySelector('.navbar');
 export const header = document.querySelector('.header');
 export const testimonialsContent = document.querySelector('div.testimonials__content');
+export const languageSelect = document.querySelector('#language-select');
 
 let interval = null;
 
